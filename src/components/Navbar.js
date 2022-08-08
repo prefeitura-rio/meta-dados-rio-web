3d3812525ed33373975c7ba4f8ed0a82414cd4f8
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-08-08 14:08:15 -0300

    chore: remove commented code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -119,28 +119,6 @@ const Navbar = () => {
             : guestLinks.userManagement}
         </ul>
       </div>
-
-      {/* <div className="container-fluid">
-        <Link href="/">
-          <a className="navbar-brand">Metadados Rio</a>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {isAuthenticated ? authLinks.navigation : guestLinks}
-          </ul>
-        </div>
-      </div> */}
     </nav>
   );
 };
