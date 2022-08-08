a5905049b0d040397495ed9f25553576838687e0
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-08-08 14:00:40 -0300

    chore: refatoração da navbar #2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,57 +15,112 @@ const Navbar = () => {
     }
   };
 
-  const authLinks = (
-    <>
-      <li className="nav-item">
-        <Link href="/dashboard">
-          <a
-            className={
-              router.pathname === "/dashboard" ? "nav-link active" : "nav-link"
-            }
-          >
-            Dashboard
+  const authLinks = {
+    navigation: (
+      <>
+        <li className="nav-item">
+          <Link href="/dashboard">
+            <a
+              className={
+                router.pathname === "/dashboard"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Dashboard
+            </a>
+          </Link>
+        </li>
+      </>
+    ),
+    userManagement: (
+      <>
+        {" "}
+        <li className="nav-item">
+          <a className="nav-link" href="#!" onClick={logoutHandler}>
+            Logout
           </a>
-        </Link>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#!" onClick={logoutHandler}>
-          Logout
-        </a>
-      </li>
-    </>
-  );
+        </li>
+      </>
+    )
+  };
 
-  const guestLinks = (
-    <>
-      <li className="nav-item">
-        <Link href="/register">
-          <a
-            className={
-              router.pathname === "/register" ? "nav-link active" : "nav-link"
-            }
-          >
-            Register
-          </a>
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link href="/login">
-          <a
-            className={
-              router.pathname === "/login" ? "nav-link active" : "nav-link"
-            }
-          >
-            Login
-          </a>
-        </Link>
-      </li>
-    </>
-  );
+  const guestLinks = {
+    navigation: (
+      <>
+        <li className="nav-item">
+          <Link href="/">
+            <a
+              className={
+                router.pathname === "/" ? "nav-link active" : "nav-link"
+              }
+            >
+              Home
+            </a>
+          </Link>
+        </li>
+      </>
+    ),
+    userManagement: (
+      <>
+        <li className="nav-item">
+          <Link href="/register">
+            <a
+              className={
+                router.pathname === "/register" ? "nav-link active" : "nav-link"
+              }
+            >
+              Register
+            </a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/login">
+            <a
+              className={
+                router.pathname === "/login" ? "nav-link active" : "nav-link"
+              }
+            >
+              Login
+            </a>
+          </Link>
+        </li>
+      </>
+    )
+  };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul className="navbar-nav me-auto">
+          {isAuthenticated ? authLinks.navigation : guestLinks.navigation}
+        </ul>
+      </div>
+      <div className="mx-auto order-0">
+        <Link href="/">
+          <a className="navbar-brand mx-auto">Metadados Rio</a>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target=".dual-collapse2"
+          aria-controls=".dual-collapse2"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul className="navbar-nav ms-auto">
+          {isAuthenticated
+            ? authLinks.userManagement
+            : guestLinks.userManagement}
+        </ul>
+      </div>
+
+      {/* <div className="container-fluid">
         <Link href="/">
           <a className="navbar-brand">Metadados Rio</a>
         </Link>
@@ -82,21 +137,10 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link href="/">
-                <a
-                  className={
-                    router.pathname === "/" ? "nav-link active" : "nav-link"
-                  }
-                >
-                  Home
-                </a>
-              </Link>
-            </li>
-            {isAuthenticated ? authLinks : guestLinks}
+            {isAuthenticated ? authLinks.navigation : guestLinks}
           </ul>
         </div>
-      </div>
+      </div> */}
     </nav>
   );
 };
--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const SECURE_COOKIES = process.env.NODE_ENV === "production";
-export const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME;
+export const SITE_NAME = "Metadados Rio";
