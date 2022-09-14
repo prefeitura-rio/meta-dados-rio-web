097bcb675f8ac3cf4e7a853c482bbe65d504bc37
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-09-14 16:21:48 -0300

    chore: Added var fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config}  767*/
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Inter': 'Inter, sans-serif',
+        'Poppins': 'Poppins, sans-serif'
+      },
+    },
+    screens: {
+      '2sm': {'max':'767px'} 
+    },
   },
   plugins: [],
 }
