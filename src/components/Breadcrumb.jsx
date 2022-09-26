89e75fd734c31f464ff27240814d67a4e04a9825
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-09-26 17:35:06 -0300

    chore: remove useless log

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -3,8 +3,6 @@ import { Children, Fragment } from "react";
 const Breadcrumb = ({ children }) => {
   const childrenArray = Children.toArray(children);
 
-  console.log(childrenArray);
-
   const childrenWtihSeperator = childrenArray.map((child, index) => {
     if (index !== childrenArray.length - 1) {
       return (
