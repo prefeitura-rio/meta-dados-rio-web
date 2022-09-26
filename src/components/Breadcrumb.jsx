e1b1725fbebaa7d9a3a40abdae3d29d18240fd5f
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-09-26 15:47:56 -0300

    feat: add breadcrumb navigation

---
+++ src/components/Breadcrumb.jsx
@@ -0,0 +1,27 @@
+import { Children, Fragment } from "react";
+
+const Breadcrumb = ({ children }) => {
+  const childrenArray = Children.toArray(children);
+
+  console.log(childrenArray);
+
+  const childrenWtihSeperator = childrenArray.map((child, index) => {
+    if (index !== childrenArray.length - 1) {
+      return (
+        <Fragment key={index}>
+          {child}
+          <span>/</span>
+        </Fragment>
+      );
+    }
+    return child
+  });
+
+  return (
+    <nav className="mx-8 md:mx-16 lg:mx-32 mt-8">
+      <ol className="flex items-center space-x-4">{childrenWtihSeperator}</ol>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { check_auth_status } from "../actions/auth";
 import { SITE_NAME } from "../config";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+import Breadcrumb from "../components/Breadcrumb";
+import BreadcrumbItem from "../components/BreadcrumbItem";
 
 const Layout = ({ pageName, content, children }) => {
+  const router = useRouter();
+  const [breadcrumbs, setBreadcrumbs] = useState();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,6 +18,25 @@ const Layout = ({ pageName, content, children }) => {
       dispatch(check_auth_status());
     }
   }, [dispatch]);
+
+  useEffect(() => {
+    if (router && router !== null && router !== undefined) {
+      // Adapted from
+      // https://blog.anishde.dev/making-an-accessible-breadcrumb-navigation-using-tailwindcss-and-nextjs
+      const pathWithoutQuery = router.asPath.split("?")[0];
+      let pathArray = pathWithoutQuery.split("/");
+      pathArray.shift();
+      pathArray = pathArray.filter((path) => path !== "");
+      const breadcrumbs = pathArray.map((path, index) => {
+        const href = "/" + pathArray.slice(0, index + 1).join("/");
+        return {
+          href,
+          label: path.charAt(0).toUpperCase() + path.slice(1)
+        };
+      });
+      setBreadcrumbs(breadcrumbs);
+    }
+  }, [router]);
 
   return (
     <>
@@ -27,6 +51,15 @@ const Layout = ({ pageName, content, children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Navbar />
+      <Breadcrumb>
+        <BreadcrumbItem href="/">Home</BreadcrumbItem>
+        {breadcrumbs &&
+          breadcrumbs.map((breadcrumb) => (
+            <BreadcrumbItem key={breadcrumb.href} href={breadcrumb.href}>
+              {breadcrumb.label}
+            </BreadcrumbItem>
+          ))}
+      </Breadcrumb>
       <div className="container mt-5">{children}</div>
     </>
   );
